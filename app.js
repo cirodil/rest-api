8d89cd6f44001808b28fd446fba707840173ddc4
cirodil <cirodil@gmail.com>
2020-04-23 22:42:46 +0300

    add post

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const path = require('path')
+const {v4} = require('uuid')
 const app = express()
 
 const CONTACTS = [
-  {id: 1, name: 'Василий', value: '8-800-555-35-35', marked: false}
+  {id: v4(), name: 'Василий', value: '8-800-555-35-35', marked: false}
 ]
+
+app.use(express.json())
 
 app.get('/api/contacts', (req, res) => {
   res.status(200).json(CONTACTS)
+})
+
+app.post('/api/contacts', (req, res) => {
+  const contact = {...req.body, id: v4(), marked: false}
+  CONTACTS.push(contact)
+  res.status(201).json(contact)
 })
 
 app.use(express.static(path.resolve(__dirname, 'client')))
